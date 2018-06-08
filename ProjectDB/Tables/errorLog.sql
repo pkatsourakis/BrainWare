a922c88a85ea5885038bc7d9f3afabffc2f5ebc9
pkatsourakis <pkatsourakis@yahoo.com>
2018-06-07 23:34:58 -0400

    Adding error log table

---
+++ ProjectDB/Tables/errorLog.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[errorLog]
+(
+	[error_id] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [message] VARCHAR(MAX) NULL, 
+    [stacktrace] VARCHAR(MAX) NULL, 
+    [source] VARCHAR(50) NULL, 
+    [date] DATETIME NULL DEFAULT GETDATE()
+)
